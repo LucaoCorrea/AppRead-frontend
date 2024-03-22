4eb0e7bfa37a55a2cd1585577991a3bfc46bf7d3
LucaoCorrea <lucascorrea1505@gmail.com>
2024-03-22 16:48:14 -0300

    page register started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -9,43 +8,42 @@ import { AllBooksComponent } from './components/pages/all-books/all-books.compon
 import { SearchedBooksComponent } from './components/pages/searched-books/searched-books.component';
 import { DescComponent } from './components/desc/desc.component';
 
-
 export const routes: Routes = [
-    {
+  {
+    path: 'login',
+    title: 'Zoe | Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    title: 'Zoe | Register',
+    component: RegisterComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
         path: '',
-        component: LayoutComponent,
-        children: [
-            {
-                path: '',
-                title: 'Zoe | Home',
-                component: AllBooksComponent, 
-            },
-            {
-                path: '',
-                title: 'Search - Books',
-                component: SearchedBooksComponent,
-                pathMatch: 'full', 
-            },
-            {
-              path: 'book/:id',
-              title: 'Zoe | Descrição',
-              component: DescComponent,
-            },
-            {
-              path: 'profile',
-              title: 'Zoe | Perfil',
-              component: ProfileComponent,
-            },
-          ],
-        },
-        {
-          path: 'login',
-          title: 'Login',
-          component: LoginComponent,
-        },
-        {
-          path: 'register',
-          title: 'Register',
-          component: RegisterComponent,
-        },
+        title: 'Zoe | Home',
+        component: AllBooksComponent,
+      },
+      {
+        path: '',
+        title: 'Search - Books',
+        component: SearchedBooksComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'book/:id',
+        title: 'Zoe | Descrição',
+        component: DescComponent,
+      },
+      {
+        path: 'profile',
+        title: 'Zoe | Perfil',
+        component: ProfileComponent,
+      },
+    ],
+  },
 ];
